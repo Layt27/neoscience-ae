1cd541d3fbd74e1a22cb21b92b84467366704f50
Layt27 <laytonchetty@gmail.com>
2025-03-06 14:12:09 +0400

    feature: Create basic structure of for the home page

---
+++ neoscience_ae_website/components/home/Home.tsx
@@ -0,0 +1,12 @@
+// Imports
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="flex flex-col">
+            <div>hello home page</div>
+        </div>
+    )
+}
+
+export default Home
